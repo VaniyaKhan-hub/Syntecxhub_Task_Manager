ddce73184f059e6c17d947b5d1346895b7a4628e
VaniyaKhan-hub <vaniya1004@gmail.com>
2026-02-08 18:22:06 +0500

    Initial Commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://vaniya1004_db_user:taskmanager123@cluster0.itlrhcx.mongodb.net/task_manager').
+    await mongoose.connect('mongodb+srv://vaniya1004_db_user:taskmanager123@cluster0.itlrhcx.mongodb.net/task_manager' || process.env.DB_URL).
     then(()=>console.log('DB Connected'));
 }
